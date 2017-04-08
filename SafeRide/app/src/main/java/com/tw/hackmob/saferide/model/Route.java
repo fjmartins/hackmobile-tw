26913d52d0673f7760ac348ed19489f2b09fe1b8
Fabio Martins <fj.martins1101@gmail.com>
2017-04-08 19:34:50 -0300

    Location and Route models

--- SafeRide/app/src/main/java/com/tw/hackmob/saferide/model/Route.java
+++ SafeRide/app/src/main/java/com/tw/hackmob/saferide/model/Route.java
@@ -6,4 +6,47 @@ package com.tw.hackmob.saferide.model;
 
 public class Route {
 
+    private String owner;
+    private long time;
+    private Location from;
+    private Location to;
+
+    public Route() {}
+
+    public Route(Location from, Location to) {
+        this.from = from;
+        this.to = to;
+    }
+
+    public Location getFrom() {
+        return from;
+    }
+
+    public void setFrom(Location from) {
+        this.from = from;
+    }
+
+    public Location getTo() {
+        return to;
+    }
+
+    public void setTo(Location to) {
+        this.to = to;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    public long getTime() {
+        return time;
+    }
+
+    public void setTime(long time) {
+        this.time = time;
+    }
 }
