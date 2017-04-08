6948dc36eabc763c2610f4c04ff5b4bbb3fbb3ff
Fabio Martins <fj.martins1101@gmail.com>
2017-04-08 14:08:49 -0300

    Implementing chat

---
+++ HackathonMobileTW/app/src/main/java/com/bot/hackmobtw/hackathonmobiletw/activity/adapter/MessageAdapter.java
@@ -0,0 +1,79 @@
+package com.bot.hackmobtw.hackathonmobiletw.activity.adapter;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.bot.hackmobtw.hackathonmobiletw.R;
+import com.bot.hackmobtw.hackathonmobiletw.model.Message;
+
+import java.util.List;
+
+/**
+ * Created by fjmartins on 4/8/2017.
+ */
+
+public class MessageAdapter extends BaseAdapter {
+
+    private Context context;
+    private List<Message> messagesItems;
+
+    public MessageAdapter(Context context, List<Message> navDrawerItems) {
+        this.context = context;
+        this.messagesItems = navDrawerItems;
+    }
+
+    @Override
+    public int getCount() {
+        return messagesItems.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return messagesItems.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @SuppressLint("InflateParams")
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        /**
+         * The following list not implemented reusable list items as list items
+         * are showing incorrect data Add the solution if you have one
+         * */
+
+        Message m = messagesItems.get(position);
+
+        LayoutInflater mInflater = (LayoutInflater) context
+                .getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
+
+        // Identifying the message owner
+        if (messagesItems.get(position).isSelf()) {
+            // message belongs to you, so load the right aligned layout
+            convertView = mInflater.inflate(R.layout.list_item_message_right,
+                    null);
+        } else {
+            // message belongs to other person, load the left aligned layout
+            convertView = mInflater.inflate(R.layout.list_item_message_left,
+                    null);
+        }
+
+        TextView lblFrom = (TextView) convertView.findViewById(R.id.lblMsgFrom);
+        TextView txtMsg = (TextView) convertView.findViewById(R.id.txtMsg);
+
+        txtMsg.setText(m.getContent());
+        lblFrom.setText(m.getUsername());
+
+        return convertView;
+    }
+}
